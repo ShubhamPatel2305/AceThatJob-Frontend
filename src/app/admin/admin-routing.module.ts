7605faf92e27e9932c0c6aa61e1a846d87cc0b56
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-09-08 07:57:53 +0530

    setup routing for newly added modules and components

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterGuardService } from '../services/router-guard.service';
+import { HelpDetailsComponent } from './help-details/help-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:LayoutComponent,
+  children:[{
+      path:'',
+      component:DashboardComponent
+    },{
+      path:'dashboard',
+      component:DashboardComponent,
+      canActivate:[RouterGuardService]
+    },{
+      path:'help',
+      component:HelpDetailsComponent,
+      canActivate:[RouterGuardService]
+    },{
+      path:'**',
+      component:DashboardComponent,
+      canActivate:[RouterGuardService]
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
